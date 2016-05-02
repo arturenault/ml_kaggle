8b2baec1f18402454abba513e93b7e477bd4fc91
Artur Renault <artur.renault@gmail.com>
2016-05-02 11:39:42 -0400

    Test on training data + some extra flags

---
+++ README.txt
@@ -0,0 +1,9 @@
+Flags:
+
+-o [filename]
+--output [filename]
+Runs code on test data and outputs submission file at filename
+
+-r
+Regenerates the test data. This happens either way if it's your first time
+running with -o.
--- main.py
+++ main.py
@@ -7,9 +7,9 @@ import pickle
 
 from sklearn.ensemble import AdaBoostClassifier
 
+test_fraction = 0.2
 train_filename = "data.csv"
 test_filename  = "quiz.csv"
-output_filename = "output.csv"
 
 def is_num(string):
   try:
@@ -37,7 +37,7 @@ def get_train_data():
 
     dataframe = pandas.DataFrame(data=x)
 
-    processed_data = pandas.get_dummies(dataframe)
+    processed_data = pandas.get_dummies(dataframe, drop_first=True)
 
     return processed_data, y
 
@@ -62,7 +62,7 @@ def get_test_data():
 
     dataframe = pandas.DataFrame(data=x)
 
-    processed_data = pandas.get_dummies(dataframe)
+    processed_data = pandas.get_dummies(dataframe, drop_first=True)
 
     return processed_data
 
@@ -80,42 +80,61 @@ def classify(classifier, test_data):
   predictions = [int(i) for i in predictions]
   return predictions
 
-def output(predictions):
-  with open(output_filename, "w") as output_file:
+def test(classifier, test_data, labels):
+  predictions = classify(classifier, test_data)
+  correct = 0.0
+  for i, prediction in enumerate(predictions):
+    if prediction == labels[i]:
+      correct += 1
+  return correct / len(labels)
+
+def output(predictions, filename):
+  with open(filename, "w") as output_file:
     writer = csv.writer(output_file)
-    writer.writerow(["id", "Prediction"])
+    writer.writerow(["Id", "Prediction"])
     for i in range(len(predictions)):
       writer.writerow([str(i + 1), predictions[i]])
-
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("-r", "--regenerate", action="store_true")
+  parser.add_argument("-o", "--output", action="store")
   args = parser.parse_args()
 
   print "getting training data"
   train_data, labels = get_train_data()
 
+  train_number = int(len(train_data) * (1 - test_fraction))
+  test_number = len(train_data) - train_number
+
   print "training"
-  classifier = train(train_data, labels)
+  classifier = train(train_data.head(train_number), labels[:train_number])
 
-  if isfile("test_data.pickle") and not args.regenerate:
-    print "loading test data"
-    test_data = pickle.load(open("test_data.pickle"))
+  print "testing"
+  score = test(classifier, train_data.tail(test_number), labels[train_number:])
 
-  else:
-    print "getting test data"
-    test_data = get_test_data()
+  print "Score: " + str(score)
 
-    print "fixing test data"
-    fix_test_data(train_data, test_data)
+  if args.output is not None:
+    if isfile("test_data.pickle") and not args.regenerate:
+      print "loading test data"
+      test_data = pickle.load(open("test_data.pickle"))
 
+    else:
+      print "getting test data"
+      test_data = get_test_data()
 
-  print "classifying"
-  predictions = classify(classifier, test_data)
+      print "fixing test data"
+      fix_test_data(train_data, test_data)
 
-  print "outputting"
-  output(predictions)
+      print "pickling test data"
+      pickle.dump(test_data, open("test_data.pickle", "w"))
+
+    print "classifying"
+    predictions = classify(classifier, test_data)
+
+    print "outputting"
+    output(predictions, args.output)
 
 
 
