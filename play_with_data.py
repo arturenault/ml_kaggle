d84c784903a57bd364b3a2932bf4a3e9f83d909c
itorras13 <itorras13@gmail.com>
2016-05-05 22:54:30 -0400

    play with data is the one that got us highest score

--- play_with_data.py
+++ play_with_data.py
@@ -5,105 +5,105 @@ from operator import itemgetter
 from random import shuffle
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import VotingClassifier
 from sklearn import preprocessing
+from sklearn.svm import SVC
+from sklearn import tree
+import time
 
 train_filename = "data.csv"
+test_filename = "quiz.csv"
+output_filename = 'VotingClassifier.csv'
 
-def main():
-	with open(train_filename) as train_file:
-		reader = csv.reader(train_file)
-		series = reader.next()
-		x = []
-		y = []
+def train():
+	t0 = time.time()
+	print t0
+	with open(train_filename) as train_file, open(test_filename) as test_file, open(output_filename, 'w') as out_file:
+		reader1 = csv.reader(train_file)
+		reader2 = csv.reader(test_file)
+		series = reader1.next()
+		series2 = reader2.next()
+		x_train = []
+		y_train = []
 
-		for row in reader:
-			x.append(row[:-1])
-			y.append(float(row[-1]))
+		x_test = []
 
-		size = len(x)
+		for row in reader1:
+			x_train.append(row[:-1])
+			y_train.append(float(row[-1]))
+
+		for row in reader2:
+			x_test.append(row)
+
+		size = len(x_train)
 		
-		new_x = []
+		x = x_train + x_test
 		le = preprocessing.LabelEncoder()
 		column_count = len(x[0])
 		for i in range(0,column_count):
 			current_col = [row[i] for row in x]
 			le.fit(current_col)
 			new_column = le.transform(current_col)
-			for index in range(0,size):
+			for index in range(0,len(x)):
 				x[index][i] = new_column[index]
 
 		# enc = OneHotEncoder()
-		# x = enc.fit_transform(x)
-		# pdb.set_trace()
+		# enc.fit(x)
+		# x = enc.transform(x).toarray()
+
+		x_train = x[:size]
+		x_test = x[size:]
 
 		split_number = int(round(size * .8))
-		training_x = x[:split_number]
-		test_x = x[split_number:]
-		training_y = y[:split_number]
-		test_y = y[split_number:]
+		training_x = x_train[:split_number]
+		test_x = x_train[split_number:]
+		training_y = y_train[:split_number]
+		test_y = y_train[split_number:]
+		# pdb.set_trace()
 
 		print "training"
-		neigh = KNeighborsClassifier()
+		# clf = tree.DecisionTreeClassifier()
+		# clf.fit(x_train, y_train)
+		neigh = VotingClassifier(voting='hard',estimators=[
+			('rf', RandomForestClassifier(n_estimators=400, n_jobs=-1)),
+			('ada', AdaBoostClassifier(base_estimator=RandomForestClassifier(n_estimators=40, n_jobs=-1)))
+			#('tree', AdaBoostClassifier(base_estimator=tree.DecisionTreeClassifier()))
+			])
 		neigh.fit(training_x,training_y)
 		print "done"
 
-		strong = []
-		percentage_used = .95
-		for i in range(0,52):
-			column_values_one = []
-			column_values_neg = []
-			for j in range(0,len(training_x)):
-				if y[j] == -1:
-					column_values_neg.append(training_x[j][i])
-				elif y[j] == 1:
-					column_values_one.append(training_x[j][i])
-			count_one = Counter(column_values_one)
-			count_neg = Counter(column_values_neg)
-			for element in set(list(count_one.elements())):
-				value_one = count_one[element]
-				value_neg = count_neg[element]
-				percentage = float(value_one)/float(value_one+value_neg)
-				if (percentage >= percentage_used or percentage <= 1-percentage_used) and value_one + value_neg > 49:
-					new_strong = {}
-					new_strong["column"] = i
-					new_strong["value"] = element
-					if percentage >= percentage_used:
-						new_strong["label"] = 1
-						new_strong["percentage"] = percentage
-					else:
-						new_strong["label"] = -1
-						new_strong["percentage"] = 1 - percentage
-					new_strong["appearances"] = value_one + value_neg
-					strong.append(new_strong)
-		sorted_strong = sorted(strong, key=itemgetter('percentage'), reverse=True)
-		length = len(sorted_strong)
-		last = sorted_strong[length-1]
+		print "predicting"
+		# predicted_y = clf.predict(x_test)
+		predicted_y = neigh.predict(test_x)
+		print "done"
 
 		correct = 0
 		wrong = 0
-		not_finish = 0
-		total = len(test_x)
-		for i in range(0,len(test_x)):
-			for classifier in sorted_strong:
-				if test_x[i][classifier['column']] == classifier['value']:
-					if test_y[i] == classifier['label']:
-						correct = correct + 1
-						break
-					else:
-						wrong = wrong + 1
-						break
-				if classifier == last:
-					predicted_y = neigh.predict([test_x[i]])
-					if test_y[i] == predicted_y[0]:
-						correct = correct + 1
-					else:
-						wrong = wrong + 1
-					# not_finish = not_finish + 1
-		print "correct " + str(correct)
-		print "wrong " + str(wrong)
-		print "didnt find " + str(not_finish)
+		for index in range(0,len(test_x)):
+			if predicted_y[index] == test_y[index]:
+				correct = correct + 1
+			else:
+				wrong = wrong + 1
+		
+		# print "correct " + str(correct)
+		# print "wrong " + str(wrong)
  		print str(float(correct)/float(correct + wrong))
- 		print str(float(correct + wrong)/float(correct + wrong + not_finish))
-		print "total " + str(total) + " " + str(correct + wrong + not_finish)
+		# print "total " + str(correct + wrong)
 
-main()
+		# print len(predicted_y)
+		# writer = csv.writer(out_file)
+		# writer.writerow(['Id','Prediction'])
+		# for index in range(0, len(predicted_y)):
+		# 	writer.writerow([index+1,int(predicted_y[index])])
+	t1 = time.time()
+	print t1-t0
+	
+train()
+
+
+
+
+
+
